a09b13689a074190e7fe3ecf7ab3a1d136dff02c
Anastasia <asya_apto@mail.ru>
2022-03-17 19:03:20 +0300

    Add django_heroku config

--- foodplan/settings.py
+++ foodplan/settings.py
@@ -11,8 +11,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 import os
+import django_heroku
+
 from environs import Env
 from pathlib import Path
+
 
 env = Env()
 env.read_env()
@@ -117,3 +120,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+django_heroku.settings(locals())
