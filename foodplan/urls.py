ba5d70425c7b3c9a3a6772a9930a31a7494976a7
Boyko Alexey <elorian@yandex.ru>
2022-03-18 15:09:20 +0300

    connected geg page

--- foodplan/urls.py
+++ foodplan/urls.py
@@ -13,7 +13,7 @@ from .views import render_order_page
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', render_start_page, name='render_start_page'),
-    #path('reg', render_reg_page, name='render_reg_page'),
+    path('reg', render_reg_page, name='render_reg_page'),
     path('auth/', include('django.contrib.auth.urls')),
     path('lk', render_lk_page, name='render_lk_page'),
     path('order', render_order_page, name='render_order_page'),
