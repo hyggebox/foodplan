deb4a210cb63f218ce814844394ae48e7a001314
Boyko Alexey <elorian@yandex.ru>
2022-03-18 13:45:02 +0300

    starting autth

--- foodplan/settings.py
+++ foodplan/settings.py
@@ -136,5 +136,5 @@ MEDIA_ROOT = BASE_DIR/'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+LOGIN_REDIRECT_URL = 'lk'
 django_heroku.settings(locals())
--- foodplan/urls.py
+++ foodplan/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -13,8 +13,8 @@ from .views import render_order_page
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', render_start_page, name='render_start_page'),
-    path('reg', render_reg_page, name='render_reg_page'),
-    path('auth', render_auth_page, name='render_auth_page'),
+    #path('reg', render_reg_page, name='render_reg_page'),
+    path('auth/', include('django.contrib.auth.urls')),
     path('lk', render_lk_page, name='render_lk_page'),
     path('order', render_order_page, name='render_order_page'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
