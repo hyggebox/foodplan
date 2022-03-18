f19d2f953cdb1f48601ffab6fa8cfb357b01cffb
itcosplay <azure_dream@mail.ru>
2022-03-18 17:17:38 +0300

    Started work with form

--- foodplan/views.py
+++ foodplan/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
+
+from .forms import SubsForm
+
 
 
 def render_start_page(request):
@@ -13,10 +18,26 @@ def render_auth_page(request):
     return render(request, 'auth.html')
 
 
+@login_required(login_url='/')
 def render_lk_page(request):
     return render(request, 'lk.html')
 
 
+@login_required(login_url='/')
 def render_order_page(request):
-    return render(request, 'order.html')
+    if request.method == "POST":
+        # name = request.POST.get("test")
+        print('==================')
+        print('POST is Here!')
+
+        return redirect('/')
+    
+    else:
+        subsform = SubsForm()
+
+        context = {
+            'form': subsform
+        }
+
+        return render(request, 'order.html', context)
 
