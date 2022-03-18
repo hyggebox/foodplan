84542582371fa85e6528a5d3b592b5b2b89181db
Boyko Alexey <elorian@yandex.ru>
2022-03-18 16:00:17 +0300

    connected reg page

---
+++ users/urls.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.views import LogoutView
+from django.urls import path
+from . import views
+
+# app_name = 'users'
+
+urlpatterns = [
+    path('signup/', views.SignUp.as_view(), name='signup')
+]
